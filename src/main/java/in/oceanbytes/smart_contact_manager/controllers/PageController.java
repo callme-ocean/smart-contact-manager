67fb61e631300d1d76ca5acfcd41d5a219837a73
sagar <sagarsingh0802@gmail.com>
2024-06-30 20:36:19 +0530

    updated annotation in controller

--- src/main/java/in/oceanbytes/smart_contact_manager/controllers/PageController.java
+++ src/main/java/in/oceanbytes/smart_contact_manager/controllers/PageController.java
@@ -1,47 +1,48 @@
 package in.oceanbytes.smart_contact_manager.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
-    @RequestMapping("/home")
+    @GetMapping("/home")
     public String home() {
         System.out.println("Home Page");
         return "home";
     }
 
     //about page
-    @RequestMapping("/about")
+    @GetMapping("/about")
     public String aboutPage() {
         System.out.println("About Page");
         return "about";
     }
 
     //service page
-    @RequestMapping("/services")
+    @GetMapping("/services")
     public String servicesPage() {
         System.out.println("Services Page");
         return "services";
     }
 
     //contact page
-    @RequestMapping("/contact")
+    @GetMapping("/contact")
     public String contactPage() {
         System.out.println("Contact Page");
         return "contact";
     }
 
     //login page
-    @RequestMapping("/login")
+    @GetMapping("/login")
     public String loginPage() {
         System.out.println("Login Page");
         return "login";
     }
 
     //register page
-    @RequestMapping("/register")
+    @GetMapping("/register")
     public String registerPage() {
         System.out.println("Register Page");
         return "register";
